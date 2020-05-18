d2a9ccdc5ba7a6e4fb748e20074af12f22a1f1b2
dbartleman <drewbartleman@gmail.com>
2020-05-17 19:51:40 -0700

    add more CRUD functionality; login page skeleton

---
+++ front-end/src/components/LoginForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    // TODO: Set up default state
+  }
+
+  render() {
+    return (
+      <form>
+        <h2>Log In</h2>
+      </form>
+    );
+  }
+};
+
+export default LoginForm;
